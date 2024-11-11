f0d4b55cfeb29e280cf4097df20360e759b8306a
Karaatsar <kara.atsar@helsinki.fi>
2024-11-11 14:39:09 +0200

    print muutoksia

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -8,8 +8,8 @@ logger("aloitetaan ohjelma") #muutoksia tehty
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"{x} + {y} = {summa(x, y)}")
+print(f"{x} - {y} = {erotus(x, y)}") #näitä muuteltiin
 
 logger("lopetetaan")
 print("goodbye!") #lisäys
