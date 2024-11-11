c61fbde87390cc1386e5e5a84a94cac9fc541ef9
Karaatsar <kara.atsar@helsinki.fi>
2024-11-11 12:39:22 +0200

    vieläkin

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -9,7 +9,17 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
+        print("Alkuperäisen tiedoston sisältö: ")
         print(content)
+        print("dererialisoitu data:")
+        print(data)
+        
+        project = Project(
+             name=data["name"], 
+             description=data["description"],
+             members=data["members"],
+             tasks=data["tasks"]
+       )
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return project	
