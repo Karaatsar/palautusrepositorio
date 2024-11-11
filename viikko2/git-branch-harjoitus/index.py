b39ffa0bc8fb22790619a05fdfb06f01c9408f7f
Karaatsar <kara.atsar@helsinki.fi>
2024-11-11 17:37:16 +0200

    mitä tapahtuu

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -8,19 +8,13 @@ logger("aloitetaan ohjelma") #muutoksia tehty
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-<<<<<<< HEAD
 print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")
 print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}") #näihinkin muutoksia
-=======
 print(f"{x} + {y} = {summa(x, y)}")
 print(f"{x} - {y} = {erotus(x, y)}") #näitä muuteltiin
->>>>>>> main
 
 logger("lopetetaan")
 print("goodbye!") #lisäys
 
-<<<<<<< HEAD
 #täällähän tapahtuu vaikka ja mitä
-=======
 #YRITÄN TEHDÄ TEHTÄVÄÄ 9
->>>>>>> 56dbd54 (lisää 9 tehtävää)
